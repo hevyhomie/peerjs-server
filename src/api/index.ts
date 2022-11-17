8805d0eb3b28609a4a3369d43770e50b21810dc4
hevyhomie <fardil.pirani@gmail.com>
2022-11-17 15:26:45 +0000

    cors origin

--- src/api/index.ts
+++ src/api/index.ts
@@ -10,13 +10,16 @@ export const Api = ({ config, realm }: {
   realm: IRealm;
 }): express.Router => {
   const app = express.Router();
-  app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
   
-  app.options('*', cors()); 
+  // app.use(function(_req, res, next) {
+  //   res.header("Access-Control-Allow-Origin", "*");
+  //   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  //   next();
+  // });
+  
+  app.use(cors({ origin: 'https://9000-hevyhomie-peerjsserver-yeprtstt9sy.ws-us74.gitpod.io' }));
+
+  // app.options('*', cors()); 
 
   app.get("/", (_, res) => {
     res.send(publicContent);
