39560e5e6688f475723b0b10c141722f00945d2e
hevyhomie <fardil.pirani@gmail.com>
2022-11-17 15:11:27 +0000

    cors option

--- src/api/index.ts
+++ src/api/index.ts
@@ -10,8 +10,13 @@ export const Api = ({ config, realm }: {
   realm: IRealm;
 }): express.Router => {
   const app = express.Router();
-
-  app.use(cors());
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  
+  app.options('*', cors()); 
 
   app.get("/", (_, res) => {
     res.send(publicContent);
