b99cd670d4144687fbfaea5f3996a9ead53f68aa
hevyhomie <fardil.pirani@gmail.com>
2022-11-19 15:38:45 +0000

    access allow origin

--- src/api/index.ts
+++ src/api/index.ts
@@ -36,7 +36,8 @@ export const Api = ({ config, realm }: {
   });
 
   app.get("/", (_, res) => {
-    
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.send(publicContent);
   });
 
