f75d95442b14982a57f29ec707e1c83dae8d1d00
hevyhomie <fardil.pirani@gmail.com>
2022-11-19 15:58:09 +0000

    original cors

--- src/api/index.ts
+++ src/api/index.ts
@@ -18,14 +18,14 @@ export const Api = ({ config, realm }: {
   // });
   
   // app.use(cors({ origin: 'https://9000-hevyhomie-peerjsserver-yeprtstt9sy.ws-us74.gitpod.io' }));
-  const corsOptions ={
-    origin:'*', 
-    // credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200,
-  }
+  // const corsOptions ={
+  //   origin:'*', 
+  //   // credentials:true,            //access-control-allow-credentials:true
+  //   optionSuccessStatus:200,
+  // }
  
-  app.use(cors(corsOptions))
-  // app.use(cors());
+  // app.use(cors(corsOptions))
+  app.use(cors());
 
   // app.options('*', cors()); 
 
