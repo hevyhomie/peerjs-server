88952e645511038cbc8d8cde1792ae2d3ab66fa8
hevyhomie <fardil.pirani@gmail.com>
2022-12-10 19:24:14 +0000

    get creds

--- src/api/index.ts
+++ src/api/index.ts
@@ -61,7 +61,7 @@ export const Api = ({ config, realm }: {
 
   app.get("/", (_, res) => {
     res.header("Access-Control-Allow-Origin", "*");
-    // res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.send(publicContent);
   });
